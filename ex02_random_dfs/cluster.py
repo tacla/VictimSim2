845e65d3f4f9727c4a34943c8fa10d8f2dbcd2a4
Maria Fernanda Azolin <azolin.mf@gmail.com>
2024-04-01 19:15:03 -0300

    clusterizacao

---
+++ ex02_random_dfs/cluster.py
@@ -0,0 +1,39 @@
+import os
+import csv
+import numpy as np
+from sklearn.cluster import KMeans
+
+class Cluster():
+
+    def __init__(self):
+        pass
+
+    def cluster(self, victims):
+        # Clustering using location and vital signs
+        victims = []
+        for victim_data in victims.values():
+            victims.append([victim_data[0][0]] + [victim_data[0][1]] + victim_data[1])
+        
+        # we are going to use 4 clusters because we have 4 rescuer agents
+        kmeans = KMeans(n_clusters=4, random_state=42)
+        kmeans.fit(X=victims)
+
+        labels = kmeans.labels_
+
+        i = 0 
+        for id, victim_data in victims.items():
+            victims[id] = (victim_data[0], victim_data[1] + [labels[i]])
+            i += 1
+
+        victims_cluster_1 = [x[0] for x in victims.values() if x[1][-1] == 0]
+        victims_cluster_2 = [x[0] for x in victims.values() if x[1][-1] == 1]
+        victims_cluster_3 = [x[0] for x in victims.values() if x[1][-1] == 2]
+        victims_cluster_4 = [x[0] for x in victims.values() if x[1][-1] == 3]        
+
+        print("*** Clustering report: ***")
+        print("Victims to be rescued by rescuer 1: {}".format(victims_cluster_1))
+        print("Victims to be rescued by rescuer 2: {}".format(victims_cluster_2))
+        print("Victims to be rescued by rescuer 3: {}".format(victims_cluster_3))
+        print("Victims to be rescued by rescuer 4: {}".format(victims_cluster_4))
+
+        return victims
--- ex02_random_dfs/main.py
+++ ex02_random_dfs/main.py
@@ -6,6 +6,7 @@ import time
 from vs.environment import Env
 from explorer import Explorer
 from rescuer import Rescuer
+from cluster import Cluster
 
 def main(data_folder_name):
    
@@ -26,17 +27,26 @@ def main(data_folder_name):
 
     # Explorer needs to know rescuer to send the map
     # that's why rescuer is instatiated before
-    exp = Explorer(env, explorer_file, resc)
-    exp2 = Explorer(env, explorer_file, resc)
-    exp3 = Explorer(env, explorer_file, resc)
-    exp4 = Explorer(env, explorer_file, resc)
-    # env.add_agent(exp)
-    # env.add_agent(exp2)
-    # env.add_agent(exp3)
-    # env.add_agent(exp4)
+    exp_1 = Explorer(env, explorer_file, resc)
+    exp_2 = Explorer(env, explorer_file, resc)
+    exp_3 = Explorer(env, explorer_file, resc)
+    exp_4 = Explorer(env, explorer_file, resc)
 
     # Run the environment simulator
     env.run()
+
+    # Clustering
+    cluster = Cluster()
+    exp_victims = {} # dictionary that will agregate all found victims
+
+    for explorer in [exp_1, exp_2, exp_3, exp_4]:
+        if explorer.victims != {}:
+            if exp_victims == {}:
+                exp_victims = explorer.victims
+            else:
+                exp_victims.update(explorer.victims)
+
+    cluster_victims = cluster.cluster(exp_victims)
     
         
 if __name__ == '__main__':
@@ -46,6 +56,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         data_folder_name = sys.argv[1]
     else:
-        data_folder_name = os.path.join("datasets", "data_132v_100x80")
+        data_folder_name = os.path.join("datasets", "data_42v_20x20")
         
     main(data_folder_name)
