9891d9b70403893c16c530a615dc56f5ef3e172c
Marcos Travasso <marcos.travasso42@gmail.com>
2024-04-10 19:51:19 -0300

    clean rescuer boss

--- en01.2/rescuer_boss.py
+++ en01.2/rescuer_boss.py
@@ -1,10 +1,9 @@
-from rescuer import Rescuer
-from sklearn.cluster import KMeans
 import numpy as np
-import os
 import pandas as pd
+from sklearn.cluster import KMeans
+
 from map import Map
-# from victim import Victim
+from rescuer import Rescuer
 
 
 class RescuerBoss(Rescuer):
@@ -14,15 +13,8 @@ class RescuerBoss(Rescuer):
         self.inactive_exp_counter = 0
         self.map = Map()
         self.map_victims = {}
-        self.map_obstacles = {}
         self.rescuers = rescuer_list
-        self.victim_clusters : list = [None,None,None,None]
-
-        self.action_cost = {
-            "N": 1, "S": 1, "E": 1, "W": 1,
-            "NE": 1.5, "SE": 1.5, "NW": 1.5, "SW": 1.5
-        }
-
+        self.victim_clusters: list = [None, None, None, None]
 
     def alert_explorer_inactive(self, map, victims):
         self.map_victims.update(victims)
@@ -37,7 +29,6 @@ class RescuerBoss(Rescuer):
         print(f"Mapa encontrado contendo {len(self.map.positions)} posições")
         self.cluster_victims()
 
-
     def cluster_victims(self) -> None:
         coordenadas = np.array([self.map_victims[id][0] for id in self.map_victims])
         features = np.array([self.map_victims[id][1] for id in self.map_victims])
@@ -45,16 +36,12 @@ class RescuerBoss(Rescuer):
         km = KMeans(n_clusters=4)
         km.fit_predict(coordenadas)
 
-        dados_clusterizados = pd.DataFrame({'ID': list(self.map_victims.keys()), 
-                                    'Coordenadas': coordenadas.tolist(), 
-                                    'Features': features.tolist(),
-                                    'Cluster': km.labels_})
-        
+        dados_clusterizados = pd.DataFrame({'ID': list(self.map_victims.keys()),
+                                            'Coordenadas': coordenadas.tolist(),
+                                            'Features': features.tolist(),
+                                            'Cluster': km.labels_})
+
         for cluster_id in range(4):
             cluster_data = dados_clusterizados[dados_clusterizados['Cluster'] == cluster_id]
             cluster_data.to_csv(f'cluster{cluster_id + 1}.csv', index=False)
             self.victim_clusters[cluster_id] = cluster_data.to_dict()
-    
-          
-        
-        
