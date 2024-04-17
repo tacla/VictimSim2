ee084bd36f6454f2c227fbc1494af1cf0df31851
Cesar Tacla <cesar.tacla@gmail.com>
2024-04-16 23:33:30 -0300

    Update main.py

--- ex03_mas_rescuers/mas/main.py
+++ ex03_mas_rescuers/mas/main.py
@@ -41,6 +41,6 @@ if __name__ == '__main__':
         data_folder_name = sys.argv[1]
     else:
         data_folder_name = os.path.join("datasets", "data_300v_90x90")
-        config_ag_folder_name = os.path.join("ex03_mas_random_dfs", "config_agnts")
+        config_ag_folder_name = os.path.join("ex03_mas_random_dfs", "cfg_1")
         
     main(data_folder_name, config_ag_folder_name)
